937599dc89268d4d971b59a79700bc709ec9df59
Eduardo Pérez <eduardojoseperezf@gmail.com>
2017-11-13 21:53:46 -0300

    Css + JS + Mockup

---
+++ Electron/app.js
@@ -0,0 +1,25 @@
+/* jshint esversion: 6 */
+
+const {app, BrowserWindow, Menu, ipcMain, dialog} = require('electron');
+const path = require('path');
+const url = require('url');
+const SerialPort = require('serialport');
+
+let MainScreen;
+
+function OpenMainScreen(){
+  MainScreen = new BrowserWindow({width: 800, height: 600, minWidth:800, minHeight:600, center:true, show:true, resizable:true});
+
+  MainScreen.loadURL(url.format({
+    pathname: path.join(__dirname, 'mainscreen.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  MainScreen.on('closed', () => {
+    MainScreen = null;
+  });
+
+}
+
+app.on('ready', OpenMainScreen);
---
+++ Electron/js/mainscreen.js
@@ -0,0 +1,38 @@
+/* jshint esversion: 6 */
+console.log("Main Screen Initialized!");
+
+function setAlert(type, message){
+  var alertType;
+  if (type == "ok") {
+    alertType = "success";
+  }
+  else if (type == "warning"){
+    alertType = "warning";
+  }
+  else if (type == "danger"){
+    alertType = "danger";
+  }
+  else{
+    alertType = "info";
+  }
+  document.getElementById("alertBox").className = `col bg-${alertType} text-white border border-dark`;
+  document.getElementById("alertMessage").textContent = message;
+}
+
+function setStatus(type, message){
+  var statusType;
+  if (type == "ok") {
+  statusType = "success";
+  }
+  else if (type == "warning"){
+    statusType = "warning";
+  }
+  else if (type == "danger"){
+    statusType = "danger";
+  }
+  else{
+    statusType = "info";
+  }
+  document.getElementById("statusBox").className = `col bg-${alertType} text-white border border-dark`;
+  document.getElementById("statusMessage").textContent = message;
+}
