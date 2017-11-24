331fae7812b82ad5bbe9274de251ce302c1bc6eb
Eduardo Pérez <eduardojoseperezf@gmail.com>
2017-11-23 21:36:21 -0300

    Send data from all sensors

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,7 +1,8 @@
-int photoPort = 0;
+int photoPort = 2;
 int tempPort = 1;
-int humPort = 2;
+int humPort = 0;
 int levelPort = 3;
+int soilPower = 7;
 int humRead = 0;
 int tempRead = 0;
 int photoRead = 0;
@@ -11,13 +12,17 @@ String incomingByte;
 void setup(){
   Serial.begin(9600);
   pinMode(13, OUTPUT);
+  pinMode(soilPower, OUTPUT);
+  digitalWrite(soilPower, LOW);
 }
 
 void loop(){
-  //humRead = analogRead(humPort);
-  humRead = 35;
-  //tempRead = analogRead(tempPort)
-  tempRead = 24;
+  humRead = readhum();
+  tempRead = analogRead(tempPort);
+  float voltage = tempRead * 5.0;
+  voltage /= 1024.0;
+  tempRead = (voltage - 0.5) * 100;
+
   photoRead = analogRead(photoPort);
   //levelRead = analogRead(levelPort);
   levelRead = 10;
@@ -38,4 +43,13 @@ void loop(){
       digitalWrite(13, HIGH);
     }
   }
+}
+
+int readhum()
+{
+  digitalWrite(soilPower, HIGH);//turn D7 "On"
+  delay(10);//wait 10 milliseconds
+  int val = analogRead(humPort);//Read the SIG value form sensor
+  digitalWrite(soilPower, LOW);//turn D7 "Off"
+  return val;//send current moisture value
 }
