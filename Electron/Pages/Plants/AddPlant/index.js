c6eee3ec5c0944d6462eba1b2cff3262209de21e
Mondmaxy <mondmaxy@gmail.com>
2017-12-02 16:23:54 -0300

    selected plant ready

--- Electron/Pages/Plants/AddPlant/index.js
+++ Electron/Pages/Plants/AddPlant/index.js
@@ -1,45 +1,55 @@
-const fs = require('fs');
+const fs = require('fs')
 
-window.onload = function () {  
+window.onload = function() {
   let basePath = __dirname.split('Electron')[0] + 'Electron'
-  
+
   let addNewPlantBtn = document.getElementById('addNewPlantBtn')
+  let backToPlantsBtn = document.getElementById('backToPlantsBtn')
   let name = document.getElementById('plantName')
   let scientificName = document.getElementById('plantScientificName')
   let image = document.getElementById('plantImage')
+
   addNewPlantBtn.addEventListener('click', () => {
     // Validation
     if (!validateNewPlant(name.value, scientificName.value, image)) {
-      alert("Some fields are empty")
+      alert('Some fields are empty')
       return
     }
     let newPlant = '\n' + name.value + ',' + scientificName.value + ',' + image.files[0].name
-    
-    fs.appendFile(basePath + '/txt/plants.txt', newPlant, function (err,data) {
+
+    fs.appendFile(basePath + '/txt/plants.txt', newPlant, function(err, data) {
       if (err) {
-        return console.log(err);
+        return console.log(err)
       }
       alert('new plant saved')
-      fs.writeFile(basePath + '/img/test.png', image.files[0].getContext('2d'), {encoding: 'binary'}, function (err, data) {
-        console.log(data);
-      })
+      fs.writeFile(
+        basePath + '/img/test.png',
+        image.files[0].getContext('2d'),
+        {encoding: 'binary'},
+        function(err, data) {
+          console.log(data)
+        }
+      )
       remote
         .getCurrentWindow()
-        .loadURL(
-          'file://' + __dirname.split('Electron')[0] + 'Electron/Pages/Plants/index.html'
-        )
+        .loadURL('file://' + __dirname.split('Electron')[0] + 'Electron/Pages/Plants/index.html')
     })
+  })
+  backToPlantsBtn.addEventListener('click', () => {
+    remote
+      .getCurrentWindow()
+      .loadURL('file://' + __dirname.split('Electron')[0] + 'Electron/Pages/Plants/index.html')
   })
 }
 
 function validateNewPlant(name, scname, image) {
-  if (name == '' || name == " " || name == null || name == undefined){
+  if (name == '' || name == ' ' || name == null || name == undefined) {
     return false
   }
-  if (scname == '' || scname == " " || scname == null || scname == undefined) {
+  if (scname == '' || scname == ' ' || scname == null || scname == undefined) {
     return false
   }
-  if (image == '' || image == " " || image == null || image == undefined) {
+  if (image == '' || image == ' ' || image == null || image == undefined) {
     return false
   }
   if (image.files[0] == undefined) {
@@ -49,15 +59,15 @@ function validateNewPlant(name, scname, image) {
 }
 
 function onFileSelected(event) {
-  var selectedFile = event.target.files[0];
-  var reader = new FileReader();
+  var selectedFile = event.target.files[0]
+  var reader = new FileReader()
 
-  var imgtag = document.getElementById("myImage");
-  imgtag.title = selectedFile.name;
+  var imgtag = document.getElementById('myImage')
+  imgtag.title = selectedFile.name
 
   reader.onload = function(event) {
-    imgtag.src = event.target.result;
-  };
+    imgtag.src = event.target.result
+  }
 
-  reader.readAsDataURL(selectedFile);
+  reader.readAsDataURL(selectedFile)
 }
--- Electron/Pages/Settings/index.js
+++ Electron/Pages/Settings/index.js
@@ -1,3 +1,31 @@
-let menuBtn = document.getElementById('menuBtn')
+const fs = require('fs')
 
-createNavbar('sys')
+window.onload = async function() {
+  let basePath = __dirname.split('Electron')[0] + 'Electron'
+  let saveBtn = document.getElementById('saveBtn')
+  let selectedPlantSelect = document.getElementById('selectedPlantSelect')
+  await fs.readFile(basePath + '/txt/plants.txt', 'utf8', function(err, data) {
+    data = data.split('\n')
+    for (let plant of data) {
+      plant = plant.split(',')[0]
+      let option = document.createElement('option')
+      option.value = plant
+      option.text = plant
+      selectedPlantSelect.appendChild(option)
+    }
+    selectedPlantSelect.value = 'Sunflower'
+    selectedPlantSelect.addEventListener('change', () => {
+      saveBtn.className = 'btn btn-primary'
+      saveBtn.addEventListener('click', () => {
+        fs.writeFile(basePath + '/txt/selectedPlant.txt', selectedPlantSelect.value, function() {
+          remote
+            .getCurrentWindow()
+            .loadURL(
+              'file://' + __dirname.split('Electron')[0] + 'Electron/Pages/Settings/index.html'
+            )
+        })
+      })
+    })
+  })
+  createNavbar('sys')
+}
